00178a3647d7514b6f91e61658f591fd9ced5c5d
iubkud <mbarry@gmail.com>
2018-01-22 23:02:09 -0500

    fixed the api urls

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: 'http://septa-reminder-backend.herokuapp.com/api/v1'
+  API_URL: '"http://septa-reminder-backend.herokuapp.com/api/v1"'
 }
